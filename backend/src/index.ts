e60b92f143c52690ceed571ce457592bd3ca5048
unknown <bequocthanh2882005@gmail.com>
2026-05-31 09:13:54 +0700

    fix: import prisma in index.ts to fix build crash

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from './db';
 import authRoutes from './routes/auth';
 import userRoutes from './routes/user';
 import campaignRoutes from './routes/campaign';
